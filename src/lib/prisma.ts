2e428daaf73c21af32153db28ff72ad13119e887
Mark LauQ <mark.lauq@gmail.com>
2025-04-25 17:12:11 +0800

    feat: setup prisma client

---
+++ src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "../app/generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+/**
+ * Creates a Prisma Client and attaches it to the global object
+ * so that only one instance of the client is created in application.
+ * This helps resolve issues with hot reloading that can occur
+ * when using Prisma ORM with Next.js in development mode.
+ */
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
